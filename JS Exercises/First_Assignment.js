e53b9ed76690ca78b3478342f8730261afe8b57a
priya-mandyal <priya.mandyal98@gmail.com>
2024-04-30 17:04:36 -0300

    Django + Python course - Udemy

---
+++ JS Exercises/First_Assignment.js
@@ -0,0 +1,35 @@
+// var weight = prompt("Please input a number inf lbs: \n");
+// console.log(typeof(weight))
+// var weightInKgs = weight * 0.54;
+
+// alert("The weight is Kgs is : " + weightInKgs);
+// console.log("Conversion Completed")
+
+//while loop
+// var i = 0;
+// while(i<5) {
+//     console.log("Hello World!");
+//     i++;
+// }
+
+//For loop
+// for(var j =0 ; j<5 ; j++) {
+//     console.log("Hello World!");
+// }
+
+//while loop - print odd number
+// var i = 0;
+
+// while(i<26) {
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+//for loop - print odd number
+// for(var i = 1; i<=25 ; i++) {
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+// }
